d54a8bc738f64b7fb0edb02728923a8afa92a8d8
James Lawrence <jlawrence2021@gmail.com>
2026-04-14 00:10:42 -0400

    work on generic grow array

--- Makefile
+++ Makefile
@@ -1,17 +1,17 @@
-build hello_world:
+build-hello_world:
 	clang++ -std=c++20 -g hello_world/src/main.cpp -o hello_world/build/main
 
-run hello_world:
-	./hello_world/build/main
-
-build grow_array:
+build-grow_array:
 	clang++ -std=c++20 -O0 -g -I grow_array/include grow_array/src/*.cpp -o grow_array/build/main
 
-run grow_array:
-	./grow_array/build/main
-
-build generic_grow_array:
+build-generic_grow_array:
 	clang++ -std=c++20 -O0 -g generic_grow_array/src/main.cpp -o generic_grow_array/build/main
 
-run generic_grow_array:
+run-hello_world:
+	./hello_world/build/main
+
+run-grow_array:
+	./grow_array/build/main
+
+run-generic_grow_array:
 	./generic_grow_array/build/main
--- generic_grow_array/src/main.cpp
+++ generic_grow_array/src/main.cpp
@@ -1,6 +1,123 @@
 #include <iostream>
+#include <stdexcept>
+
+template <typename T>
+class generic_grow_array {
+
+    int capacity_;
+    int size_;
+    T* container_;
+
+public:
+    generic_grow_array()
+    :
+    capacity_(0),
+    size_(0),
+    container_(nullptr)
+    {}
+    
+    ~generic_grow_array() {
+        delete[] container_;
+    }
+
+    void push_back(T val) {
+        if (size_==capacity_) {
+            int new_capacity = (capacity_ == 0) ? 1 : capacity_ * 2;
+            
+            T* temp = new T[new_capacity];
+            for (int i=0; i<size_; i++) {
+                temp[i] = container_[i];
+            }
+
+            delete[] container_;
+            container_ = temp;
+            capacity_ = new_capacity;
+        }
+
+        container_[size_] = val;
+        size_++;
+    }
+
+    T operator[](int i) {
+        if (i < 0 || i >= size_)
+            throw std::out_of_range("Out of range");
+        else
+            return container_[i];
+    }
+
+    const T& operator[](int i) const {
+        if (i < 0 || i >= size_)
+            throw std::out_of_range("Out of range");
+        else
+            return container_[i];
+    }
+
+    int size() const {
+        return size_;
+    }
+
+    void stats() const {
+        std::cout << "size=" << size_ << " capacity=" << capacity_ << std::endl;
+    }
+
+    void print() {
+        for (int i=0; i<size_; i++) {
+            std::cout << container_[i] << std::endl;
+        }
+        std::cout << std::endl;
+    }
+};
 
 int main() {
-    std::cout << "Hello World!" << std::endl;
+    generic_grow_array<int> a;
+
+    a.stats();
+    a.push_back(56);
+    a.stats();
+    a.push_back(92);
+    a.stats();
+    a.push_back(13);
+    a.stats();
+    a.push_back(6);
+    a.stats();
+    a.push_back(134);
+    a.stats();
+    a.push_back(92);
+    a.stats();
+    a.push_back(763);
+    a.stats();
+    a.push_back(10016);
+    a.stats();
+    a.push_back(0);
+    a.stats();
+
+    a.print();
+    a[8] = 1;
+    a.print();
+
+    generic_grow_array<int> b = a;
+    b.print();
+
+    generic_grow_array<int> c{};
+    c = b;
+    c.print();
+
+    generic_grow_array<int> d = std::move(a);
+    d.print();
+
+    generic_grow_array<int> e{};
+    e = std::move(b);
+    e.print();
+
+    // test 'moved from' objects are still safe to use
+    a.print();
+    b.stats();
+
+    // test self-assignment
+    c = c;
+    c.print();
+    c = std::move(c);
+    c.print();
+
     return 0;
 }
